0721782f5ef6e182c7aa7ff27d19bdcc6cdac56f
Remnou2 <remi.nouveau@epitech.eu>
2024-03-11 12:29:14 +0100

    fix: now free everything

--- src/event_handlers/resize_window.c
+++ src/event_handlers/resize_window.c
@@ -15,7 +15,8 @@ void resize_event(wnd_t *wnd_struct)
 {
     sfFloatRect size = {0, 0, wnd_struct->Event.size.width,
         wnd_struct->Event.size.height};
-    sfView const *visible = sfView_createFromRect(size);
+    sfView *visible = sfView_createFromRect(size);
 
     sfRenderWindow_setView(wnd_struct->wnd, visible);
+    sfView_destroy(visible);
 }
